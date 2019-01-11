92fc1b3e3fa8934f76fbe8f4417dc7fdc741d3e4
Simply007 <chrastina.ondra@gmail.com>
2019-01-11 11:43:05 +0100

    last touch fo client

--- apolloClient.js
+++ apolloClient.js
@@ -3,9 +3,9 @@ const { InMemoryCache } = require('apollo-cache-inmemory');
 const { createHttpLink } = require('apollo-link-http');
 const fetch = require('node-fetch');
 
-const { protocol, graphQLPath, host } = require('./config');
+const { port, protocol, graphQLPath, host } = require('./config');
 
-const uri = `${protocol}${host}${graphQLPath}`;
+const uri = `${protocol}${host}${process.env.NODE_ENV !== 'production' ? ':'+port : ''}${graphQLPath}`;
 const link = createHttpLink({ uri, fetch });
 const cache = new InMemoryCache();
 
